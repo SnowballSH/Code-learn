1f6a1da0a217571a22d94ed807a35883a6d2e34e
SnowballSH <derek070211@gmail.com>
2023-04-05 16:27:53 -0500

    chat

---
+++ frontend/components/ChatInterface.tsx
@@ -0,0 +1,71 @@
+import { useState, useRef, useEffect, ChangeEvent, FormEvent } from "react";
+import "./ChatInterface.css";
+
+interface ChatInterfaceProps {
+  onSendMessage: (message: string) => void;
+  messages: Array<string>;
+}
+
+function ChatInterface({ onSendMessage, messages }: ChatInterfaceProps) {
+  const [inputText, setInputText] = useState<string>("");
+
+  // reference to the chat display div
+  const chatDisplayRef = useRef<HTMLDivElement>(null);
+
+  // automatically scroll to bottom when new messages are added
+  const scrollToBottom = (): void => {
+    if (chatDisplayRef.current) {
+      chatDisplayRef.current.scrollTop = chatDisplayRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  // handle sending messages
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (inputText.trim() === "") return;
+    onSendMessage(inputText);
+    setInputText("");
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+    setInputText(e.target.value);
+  };
+
+  // Enter = submit, Shift Enter = new line
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>): void => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(e as unknown as FormEvent<HTMLFormElement>);
+    }
+  };
+
+  return (
+    <div className="chat-interface">
+      <div className="chat-display" ref={chatDisplayRef}>
+        {messages.map((message, index) => (
+          <p key={index} className="message">
+            {message}
+          </p>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="chat-input-form">
+        <textarea
+          className="chat-input"
+          value={inputText}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          placeholder="问你想问的问题..."
+        />
+        <button type="submit" className="send-button">
+          发送
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ChatInterface;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import ChatInterface from "../components/ChatInterface";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function getTask() {
+  return "目标 - 计算数组的算术平均值";
 }
 
-export default App
+function App() {
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    setTask(getTask());
+  }, []);
+
+  const handleSendMessage = (message: string) => {
+    // Process the message here, e.g., send it to ChatGPT
+    setMessages((prevMessages) => [...prevMessages, message]);
+  };
+
+  const [messages, setMessages] = useState<Array<string>>([]);
+
+  return (
+    <div className="parent">
+      <div className="chatbar">
+        <ChatInterface onSendMessage={handleSendMessage} messages={messages} />
+      </div>
+
+      <div className="task">
+        <h1>{task}</h1>
+      </div>
+
+      <div className="codearea">3</div>
+    </div>
+  );
+}
+
+export default App;
